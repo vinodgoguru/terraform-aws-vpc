a6d1ea0ed1ae45b236b281ef137600a7ed67746e
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-26 19:10:06 +0530

    first commit

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,19 @@
   value       = data.aws_availability_zones.available.names
 }
  */
+
+ output "vpc_id" {
+  value = aws_vpc.main.id   
+ }
+
+ output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+ }
+
+ output "private_subnet_ids" {
+  value = aws_subnet.public[*].id
+ }
+
+ output "database_subnet_ids" {
+  value = aws_subnet.public[*].id
+ }
