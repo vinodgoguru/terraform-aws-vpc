9cad969e80f95b42ea5ac67c68a52efdf074cf62
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-26 16:52:11 +0530

    first commit

--- data.tf
+++ data.tf
@@ -5,3 +5,7 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default" {
   default = true
 }
+
+data "aws_route_table" "default" {
+  route_table_id = data.aws_vpc.default.main_route_table_id
+}
--- peering.tf
+++ peering.tf
@@ -27,3 +27,24 @@ resource "aws_route" "public_peering" {
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
+
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "default" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
